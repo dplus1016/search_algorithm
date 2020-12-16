5c613ccb1a67eaf45f17ca59c5dad59293e2bcb7
안득하 <dplus1016@gmail.com>
2020-12-17 00:49:01 +0900

    Update 1_Hill_Climbing.py

--- 1_Hill_Climbing.py
+++ 1_Hill_Climbing.py
@@ -1,12 +1,14 @@
+# Hill Climbing algorithm for 8-puzzle problem
+# Have a great time, Good luck!
+# programming by An deukha
+# email: dplus1016@gyeongui.hs.kr
+
+
+## The code is being modified.
+
+'''
 from copy import deepcopy
 
-# t2=deepcopy(t1)
-# Tlist=[t1,t2]; s=sorted(Tlist, key=lambda x:x[0])
-
-Olist=[] # open list
-Clist=[] # close list
-nodeNum=1
-'''
 Snode=[[1,2,4],
        [5,0,7],
        [3,6,8]]
@@ -22,15 +24,15 @@ Snode=[[2,8,3],
 Gnode=[[1,2,3],
        [8,0,4],
        [7,6,5]]
-'''
 
-Snode=[[2,8,3],
-       [1,6,4],
-       [7,0,5]]
+Snode=[[1,4,2],
+       [3,0,6],
+       [7,5,8]]
 
-Gnode=[[1,2,3],
-       [4,5,6],
-       [7,8,0]]
+Gnode=[[0,1,2],
+       [3,4,5],
+       [6,7,8]]
+
 
 # 휴리스틱 정보(평가함수): 제 자리가 아닌 퍼즐의 숫자
 def evaluate(node):
@@ -42,34 +44,22 @@ def evaluate(node):
     return 9-cnt
 
 # 평가함수 결과를 포함하는 노드 생성
-def NodeCreate(node,parentsnode):
-    global nodeNum
-    Nlist=[0,[],[]]
+def NodeCreate(node,cnt):
+    Nlist=[0,[],0]
     Nlist[0]=evaluate(node)
     Nlist[1]=node
-    Nlist[2]=parentsnode
+    Nlist[2]=cnt
     return Nlist
 
-# 오픈리스트에 추가(선택된 노드의 자식 노드 추가)
-def openList(node):
-    global Olist
-    Olist.append(node)
-
-# 클로즈리스트에 선택된 노드를 추가(오픈리스트에서는 삭제)
-def closeList():
-    global Olist
-    global Clist
-    Clist.append(Olist[0][1])
-    del Olist[0]
-
 # current 노드 출력
-def NodePrint(node):
+def NodePrint(node,h):
+    print(f"h={h}")
     for i in node:
         print(i)
     print("---------")
 
 # 자식노드 생성
-def babyNode(node,parentsnode):
+def babyNode(node):
     i,j=findzero(node)
     
     leftnode=deepcopy(node)
@@ -81,16 +71,16 @@ def babyNode(node,parentsnode):
     
     if j-1>=0: 
         leftnode[i][j-1],leftnode[i][j]=leftnode[i][j],leftnode[i][j-1]
-        if leftnode!=parentsnode: totalbabynode.append(leftnode)
+        if chkNode(leftnode): totalbabynode.append(leftnode)
     if i+1<=2: 
         downnode[i+1][j],downnode[i][j]=downnode[i][j],downnode[i+1][j]
-        if downnode!=parentsnode: totalbabynode.append(downnode)
+        if chkNode(downnode): totalbabynode.append(downnode)
     if j+1<=2: 
         rightnode[i][j+1],rightnode[i][j]=rightnode[i][j],rightnode[i][j+1]
-        if rightnode!=parentsnode: totalbabynode.append(rightnode) 
+        if chkNode(rightnode): totalbabynode.append(rightnode)
     if i-1>=0: 
         upnode[i-1][j],upnode[i][j]=upnode[i][j],upnode[i-1][j]
-        if upnode!=parentsnode: totalbabynode.append(upnode)    
+        if chkNode(upnode): totalbabynode.append(upnode) 
 
     return totalbabynode
 
@@ -100,24 +90,42 @@ def findzero(node):
             if node[i][j]==0: 
                 return i,j
 
-openList(NodeCreate(Snode,[]))
+def chkNode(node):
+    
+    return True
 
-while Olist:
-    currentnode=Olist[0][1]
-    parentsnode=Olist[0][2]
-    closeList()
+def SelectNode():
+    global Olist
+    tmp=Olist[0][0]
+    i=0 
+    for O in Olist:
+        if O[0]!=tmp: break
+        i+=1
+    return rn.randint(0,i-1)
+    #return 0
+
+NodeCreate(Snode,0))
+
+cnt=0
+while 1:
+    i=SelectNode()
+    cnt+=1
+    print(f"count: {cnt}")
+    print(f"selectNode: {i}")
+
+    currentnode=Olist[i][1]  #현재 노드
+    h=Olist[i][0] 
+    g=Olist[i][2]
+    
+    closeList(i)
     if currentnode == Gnode:
+        NodePrint(currentnode,h,g)
         print("success!!")
         break
-    NodePrint(currentnode)
-    Bnodes=babyNode(currentnode,parentsnode)
+
+    NodePrint(currentnode,h,g)
+    Bnodes=babyNode(currentnode)
     for i in Bnodes:
-        openList(NodeCreate(i,currentnode))
+        openList(NodeCreate(i,g+1))
     Olist=sorted(Olist, key=lambda x:x[0])
-
-print("---close---")
-
-for i in Clist: 
-    for j in i: 
-        print(j)
-    print("--------")
+    '''
