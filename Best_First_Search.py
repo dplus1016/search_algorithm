074fc4d10b484f56851ea23fb90d41111d85c448
안득하 <dplus1016@gmail.com>
2020-12-17 00:37:28 +0900

    Update and rename best_first_search(with random).py to Best_First_Search.py

--- Best_First_Search.py
+++ Best_First_Search.py
@@ -1,10 +1,13 @@
+# Best First Search algorithm for 8-puzzle problem
+# Have a great time, Good luck!
+# programming by An deukha
+# email: dplus1016@gyeongui.hs.kr
+
 from copy import deepcopy
+import random as rn
 
-# t2=deepcopy(t1)
-# Tlist=[t1,t2]; s=sorted(Tlist, key=lambda x:x[0])
-
-Olist=[] # open list
-Clist=[] # close list
+Olist=[] # open list [0,[[]],0]
+Clist=[] # close list [[]]
 
 
 Snode=[[1,2,4],
@@ -23,13 +26,13 @@ Gnode=[[1,2,3],
        [8,0,4],
        [7,6,5]]
 
-Snode=[[2,8,3],
-       [1,6,4],
-       [7,0,5]]
+Snode=[[1,4,2],
+       [3,0,6],
+       [7,5,8]]
 
-Gnode=[[1,2,3],
-       [4,5,6],
-       [7,8,0]]
+Gnode=[[0,1,2],
+       [3,4,5],
+       [6,7,8]]
 '''
 
 # 휴리스틱 정보(평가함수): 제 자리가 아닌 퍼즐의 숫자
@@ -42,12 +45,11 @@ def evaluate(node):
     return 9-cnt
 
 # 평가함수 결과를 포함하는 노드 생성
-def NodeCreate(node,parentsnode,cnt):
-    Nlist=[0,[],[],0]
-    Nlist[0]=evaluate(node)+cnt
+def NodeCreate(node,cnt):
+    Nlist=[0,[],0]
+    Nlist[0]=evaluate(node)
     Nlist[1]=node
-    Nlist[2]=parentsnode
-    Nlist[3]=cnt
+    Nlist[2]=cnt
     return Nlist
 
 # 오픈리스트에 추가(선택된 노드의 자식 노드 추가)
@@ -56,21 +58,21 @@ def openList(node):
     Olist.append(node)
 
 # 클로즈리스트에 선택된 노드를 추가(오픈리스트에서는 삭제)
-def closeList():
+def closeList(i):
     global Olist
     global Clist
-    Clist.append(Olist[0][1])
-    del Olist[0]
+    Clist.append(Olist[i][1])
+    del Olist[i]
 
 # current 노드 출력
-def NodePrint(node,f,g):
-    print(f"h={f-g}, g={g}")
+def NodePrint(node,h,g):
+    print(f"h={h}, g={g}")
     for i in node:
         print(i)
     print("---------")
 
 # 자식노드 생성
-def babyNode(node,parentsnode):
+def babyNode(node):
     i,j=findzero(node)
     
     leftnode=deepcopy(node)
@@ -82,16 +84,16 @@ def babyNode(node,parentsnode):
     
     if j-1>=0: 
         leftnode[i][j-1],leftnode[i][j]=leftnode[i][j],leftnode[i][j-1]
-        if leftnode!=parentsnode: totalbabynode.append(leftnode)
+        if chkNode(leftnode): totalbabynode.append(leftnode)
     if i+1<=2: 
         downnode[i+1][j],downnode[i][j]=downnode[i][j],downnode[i+1][j]
-        if downnode!=parentsnode: totalbabynode.append(downnode)
+        if chkNode(downnode): totalbabynode.append(downnode)
     if j+1<=2: 
         rightnode[i][j+1],rightnode[i][j]=rightnode[i][j],rightnode[i][j+1]
-        if rightnode!=parentsnode: totalbabynode.append(rightnode) 
+        if chkNode(rightnode): totalbabynode.append(rightnode)
     if i-1>=0: 
         upnode[i-1][j],upnode[i][j]=upnode[i][j],upnode[i-1][j]
-        if upnode!=parentsnode: totalbabynode.append(upnode)    
+        if chkNode(upnode): totalbabynode.append(upnode) 
 
     return totalbabynode
 
@@ -101,29 +103,46 @@ def findzero(node):
             if node[i][j]==0: 
                 return i,j
 
-openList(NodeCreate(Snode,[],0))
+def chkNode(node):
+    global Olist
+    global Clist
+    for i in Olist:
+        if i[1] == node: return False
+    for i in Clist: 
+        if i == node: return False
+    return True
 
+def SelectNode():
+    global Olist
+    tmp=Olist[0][0]
+    i=0 
+    for O in Olist:
+        if O[0]!=tmp: break
+        i+=1
+    return rn.randint(0,i-1)
+    #return 0
 
-while Olist:
-    currentnode=Olist[0][1]
-    parentsnode=Olist[0][2]
-    f=Olist[0][0]
-    g=Olist[0][3]
+openList(NodeCreate(Snode,0))
+
+cnt=0
+while 1:
+    i=SelectNode()
+    cnt+=1
+    print(f"count: {cnt}")
+    print(f"selectNode: {i}")
+
+    currentnode=Olist[i][1]  #현재 노드
+    h=Olist[i][0] 
+    g=Olist[i][2]
     
-    closeList()
+    closeList(i)
     if currentnode == Gnode:
+        NodePrint(currentnode,h,g)
         print("success!!")
         break
 
-    NodePrint(currentnode,f,g)
-    Bnodes=babyNode(currentnode,parentsnode)
+    NodePrint(currentnode,h,g)
+    Bnodes=babyNode(currentnode)
     for i in Bnodes:
-        openList(NodeCreate(i,currentnode,g+1))
+        openList(NodeCreate(i,g+1))
     Olist=sorted(Olist, key=lambda x:x[0])
-
-print("---close---")
-
-for i in Clist: 
-    for j in i: 
-        print(j)
-    print("--------")
