b4c78f8d180a0a962b1c6240d8cafb85edff1509
안득하 <dplus1016@gmail.com>
2020-12-27 15:06:25 +0900

    Update 1_Hill_Climbing.py

--- 1_Hill_Climbing.py
+++ 1_Hill_Climbing.py
@@ -1,22 +1,15 @@
-# Hill Climbing algorithm for 8-puzzle problem
-# Have a great time, Good luck!
-# programming by An deukha
-# email: dplus1016@gyeongui.hs.kr
-
-
-## The code is being modified.
-
-'''
 from copy import deepcopy
 
-Snode=[[1,2,4],
-       [5,0,7],
-       [3,6,8]]
+# solvable initial board
+Snode=[[1,2,3],
+       [0,4,6],
+       [7,5,8]]
 
-Gnode=[[1,4,7],
-       [2,5,8],
-       [3,6,0]]
-
+Gnode=[[1,2,3],
+       [4,5,6],
+       [7,8,0]]
+'''
+# unsolvable initial board
 Snode=[[2,8,3],
        [1,6,4],
        [7,0,5]]
@@ -24,16 +17,7 @@ Snode=[[2,8,3],
 Gnode=[[1,2,3],
        [8,0,4],
        [7,6,5]]
-
-Snode=[[1,4,2],
-       [3,0,6],
-       [7,5,8]]
-
-Gnode=[[0,1,2],
-       [3,4,5],
-       [6,7,8]]
-
-
+'''
 # 휴리스틱 정보(평가함수): 제 자리가 아닌 퍼즐의 숫자
 def evaluate(node):
     cnt=0
@@ -42,14 +26,6 @@ def evaluate(node):
             if node[i][j]==Gnode[i][j]:
                 cnt+=1
     return 9-cnt
-
-# 평가함수 결과를 포함하는 노드 생성
-def NodeCreate(node,cnt):
-    Nlist=[0,[],0]
-    Nlist[0]=evaluate(node)
-    Nlist[1]=node
-    Nlist[2]=cnt
-    return Nlist
 
 # current 노드 출력
 def NodePrint(node,h):
@@ -71,16 +47,16 @@ def babyNode(node):
     
     if j-1>=0: 
         leftnode[i][j-1],leftnode[i][j]=leftnode[i][j],leftnode[i][j-1]
-        if chkNode(leftnode): totalbabynode.append(leftnode)
+        totalbabynode.append(leftnode)
     if i+1<=2: 
         downnode[i+1][j],downnode[i][j]=downnode[i][j],downnode[i+1][j]
-        if chkNode(downnode): totalbabynode.append(downnode)
+        totalbabynode.append(downnode)
     if j+1<=2: 
         rightnode[i][j+1],rightnode[i][j]=rightnode[i][j],rightnode[i][j+1]
-        if chkNode(rightnode): totalbabynode.append(rightnode)
+        totalbabynode.append(rightnode) 
     if i-1>=0: 
         upnode[i-1][j],upnode[i][j]=upnode[i][j],upnode[i-1][j]
-        if chkNode(upnode): totalbabynode.append(upnode) 
+        totalbabynode.append(upnode)    
 
     return totalbabynode
 
@@ -90,42 +66,28 @@ def findzero(node):
             if node[i][j]==0: 
                 return i,j
 
-def chkNode(node):
-    
-    return True
-
-def SelectNode():
-    global Olist
-    tmp=Olist[0][0]
-    i=0 
-    for O in Olist:
-        if O[0]!=tmp: break
-        i+=1
-    return rn.randint(0,i-1)
-    #return 0
-
-NodeCreate(Snode,0))
-
 cnt=0
-while 1:
-    i=SelectNode()
+currentnode=Snode
+while True:
     cnt+=1
+    h=evaluate(currentnode)
+    NodePrint(currentnode,h)
     print(f"count: {cnt}")
-    print(f"selectNode: {i}")
 
-    currentnode=Olist[i][1]  #현재 노드
-    h=Olist[i][0] 
-    g=Olist[i][2]
-    
-    closeList(i)
     if currentnode == Gnode:
-        NodePrint(currentnode,h,g)
         print("success!!")
         break
-
-    NodePrint(currentnode,h,g)
+    
     Bnodes=babyNode(currentnode)
+    min_h=h
+    tmp_node=currentnode
     for i in Bnodes:
-        openList(NodeCreate(i,g+1))
-    Olist=sorted(Olist, key=lambda x:x[0])
-    '''
+        tmp_h=evaluate(i)
+        if min_h>tmp_h:
+            min_h=tmp_h
+            tmp_node=i
+    if currentnode == tmp_node:
+        print("failure!!")
+        break
+
+    currentnode = tmp_node
